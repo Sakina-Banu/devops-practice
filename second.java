ced042ea5ebe84b70f6e6b33d9cce8d4b4e5724c
Sakina Banu <115139805+hibatanveer@users.noreply.github.com>
2025-07-09 23:16:02 +0530

    Update second.java

--- second.java
+++ second.java
@@ -1,5 +1,5 @@
 this is the second file doing
 
 updating the code to development branch changes
-this line of code upade in master branch
+this line of code upadate in master branch
 this code is upadte on development branch 
